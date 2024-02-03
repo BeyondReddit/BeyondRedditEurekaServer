e175a686e1b8b6ba5476d3ae33f24e3ec4db804b
fuqing <fuqingreal@gmail.com>
2024-02-03 13:16:35 -0500

    Initial commit

---
+++ src/test/java/com/example/beyondredditeurekaserver/BeyondRedditEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beyondredditeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondRedditEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
